2002c9a2e1991741d582f170325738ad1f4ca4f7
minarin0179 <takissyd@gmail.com>
2023-10-07 20:59:12 +0900

    layer3を削除、レイヤー順を変更

--- my_keymap/config.h
+++ my_keymap/config.h
@@ -34,7 +34,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #endif
 
 #define TAP_CODE_DELAY 5
+#define TAPPING_TERM 100
 
 #define POINTING_DEVICE_AUTO_MOUSE_ENABLE
-#define AUTO_MOUSE_DEFAULT_LAYER 4
+#define AUTO_MOUSE_DEFAULT_LAYER 1
 #define AUTO_MOUSE_TIME 2000
